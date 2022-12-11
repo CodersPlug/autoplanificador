4164fd35217167f139e14bc0734a7cf892f62053
Leo Leon <CodersPlug@users.noreply.github.com>
2022-12-11 10:50:43 -0300

    Más operadores agregados

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       lat: "-34.55884338552234",
       lng: "-58.41211645073693",
       operadorID: "3982",
-      operadorNombre: "Guido Jerez",
+      operadorNombre: "Guido Gil",
       estado: "Despachando",
       litros: "19,2 lts"
     },
@@ -24,7 +24,7 @@ function App() {
       lat: "-34.55784338552234",
       lng: "-58.41111645073693",
       operadorID: "2756",
-      operadorNombre: "Juan Menebdez",
+      operadorNombre: "Marcelo Menendez",
       estado: "Disponible",
       litros: "6,2 lts"
     },
@@ -33,7 +33,7 @@ function App() {
       lat: "-34.55984338552234",
       lng: "-58.41511645073693",
       operadorID: "4149",
-      operadorNombre: "Fabian Medina",
+      operadorNombre: "Fabian Fudo",
       estado: "Disponible",
       litros: "1,2 lts"
     },
@@ -42,9 +42,45 @@ function App() {
       lat: "-34.55784338552234",
       lng: "-58.41411645073693",
       operadorID: "3730",
-      operadorNombre: "Juan Perez",
+      operadorNombre: "Juan Juarez",
       estado: "Disponible",
       litros: "19,8 lts"
+    },
+    {
+      unidad: "A-06",
+      lat: "-34.55884338552234",
+      lng: "-58.41311645073693",
+      operadorID: "3735",
+      operadorNombre: "Carlos Coronel",
+      estado: "Disponible",
+      litros: "9,1 lts"
+    },
+    {
+      unidad: "A-07",
+      lat: "-34.56084338552234",
+      lng: "-58.41011645073693",
+      operadorID: "3731",
+      operadorNombre: "Juan Jaime",
+      estado: "Despachando",
+      litros: "7,2 lts"
+    },
+    {
+      unidad: "A-08",
+      lat: "-34.55884338552234",
+      lng: "-58.41801645073693",
+      operadorID: "3729",
+      operadorNombre: "Gabriel Gomez",
+      estado: "Disponible",
+      litros: "1,9 lts"
+    },
+    {
+      unidad: "A-09",
+      lat: "-34.55684338552234",
+      lng: "-58.41511645073693",
+      operadorID: "3725",
+      operadorNombre: "Antonio Alonso",
+      estado: "Despachando",
+      litros: "17,3 lts"
     }
   ];
 
